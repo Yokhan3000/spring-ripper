8182ea596255fd7fa2ff6c90720454112f1aaca7
Mikhail Chtetsov <chtetsoff@gmail.com>
2017-12-03 18:33:20 +0300

    ProfilingHandlerBeanPostProcessor, add proxy

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -8,7 +8,13 @@ public class Main {
   public static void main(String[] args) {
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
         "spring-config.xml");
-    context.getBean(TerminatorQuoter.class).sayQuote();
+    while (true) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      context.getBean(Quoter.class).sayQuote();
+    }
   }
-
 }
